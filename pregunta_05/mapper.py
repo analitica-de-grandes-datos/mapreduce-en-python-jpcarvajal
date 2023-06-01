a4caf54cc465bf09019d4a69cb7b2b99cf1c5fec
Juan Pablo <jpcarvajalga@unal.edu.co>
2023-06-01 15:47:07 -0500

    a

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,8 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+#! /usr/bin/ python3
+
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        mes=line.split("   ")[1][5:7]
+        sys.stdout.write("{}\t1\n".format(mes))    
